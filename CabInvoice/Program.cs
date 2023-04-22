f579584e31b073cd806a4ee2d418f07b46fa9f5c
unknown <satya-1298>
2023-04-22 18:22:04 +0530

    Greetings

---
+++ CabInvoice/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CabInvoice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+
+            Console.WriteLine(fare);
+        }
+    }
+}
